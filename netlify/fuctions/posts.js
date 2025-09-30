5fbb6b4c68ec92aee4b647a66a2dc23639d587f2
Siyamthanda Amahle Buthelezi <siyamthanda917@gmail.com>
2025-09-30 07:58:09 +0200

    Update posts.js

--- netlify/fuctions/posts.js
+++ netlify/fuctions/posts.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.NETLIFY_DATABASE_URL,
   ssl: { rejectUnauthorized: false }
 });
 
@@ -32,4 +32,5 @@ exports.handler = async (event, context) => {
   } catch (error) {
     return { statusCode: 500, body: error.toString() };
   }
+
 };
